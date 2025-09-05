4c97f18dd5df17454f2681349d7c532ada9ea2b7
William Andrian Dharma T <williamadt123@gmail.com>
2025-09-05 09:54:16 +0700

    feat: RBF Kernel

--- src/supervised-learning/svm.py
+++ src/supervised-learning/svm.py
@@ -7,22 +7,55 @@ from sklearn import datasets
 
 
 class BinarySVMClassifier:
-    def __init__(self, learning_rate=0.001, lambda_param=0.01, n_iters=1000):
+    def __init__(
+        self,
+        learning_rate=0.001,
+        lambda_param=0.01,
+        n_iters=1000,
+        kernel="linear",
+        gamma=0.5,
+    ):
         self.lr = learning_rate
         self.lambda_param = lambda_param
         self.n_iters = n_iters
+        self.kernel = kernel
+        self.gamma = gamma
         self.w = None
         self.b = None
+        self.X_train = None
+        self.y_train = None
 
     def fit(self, X, y):
         n_samples, n_features = X.shape
+        self.X_train = X
+        self.y_train = y
+        if self.kernel == "linear":
+            self.w = np.zeros(n_features)
+            self.b = 0
+            for _ in range(self.n_iters):
+                for i, x_i in enumerate(X):
+                    self._update_weights(i, x_i, y)
+        elif self.kernel == "rbf":
+            self.alpha = np.zeros(n_samples)
+            self.b = 0
+            K = self._compute_kernel_matrix(X)
+            for _ in range(self.n_iters):
+                for i in range(n_samples):
+                    condition = y[i] * (np.sum(self.alpha * y * K[:, i]) + self.b) < 1
+                    if condition:
+                        self.alpha[i] += self.lr
+                        self.b += self.lr * y[i]
 
-        self.w = np.zeros(n_features)
-        self.b = 0
+    def _rbf_kernel(self, x1, x2):
+        return np.exp(-self.gamma * np.linalg.norm(x1 - x2) ** 2)
 
-        for _ in range(self.n_iters):
-            for i, x_i in enumerate(X):
-                self._update_weights(i, x_i, y)
+    def _compute_kernel_matrix(self, X):
+        n_samples = X.shape[0]
+        K = np.zeros((n_samples, n_samples))
+        for i in range(n_samples):
+            for j in range(n_samples):
+                K[i, j] = self._rbf_kernel(X[i], X[j])
+        return K
 
     def _update_weights(self, i, x_i, y):
         # margin check valid or not
@@ -34,15 +67,38 @@ class BinarySVMClassifier:
             self.b += self.lr * y[i]
 
     def predict(self, X):
-        approx = np.dot(X, self.w) + self.b
-        return np.where(approx >= 0, 1, -1)
+        if self.kernel == "linear":
+            approx = np.dot(X, self.w) + self.b
+            return np.where(approx >= 0, 1, -1)
+        elif self.kernel == "rbf":
+            y_pred = []
+            for x in X:
+                s = 0
+                for i in range(len(self.X_train)):
+                    s += (
+                        self.alpha[i]
+                        * self.y_train[i]
+                        * self._rbf_kernel(self.X_train[i], x)
+                    )
+                s += self.b
+                y_pred.append(1 if s >= 0 else -1)
+            return np.array(y_pred)
 
 
 class SVMClassifier:
-    def __init__(self, learning_rate=0.001, lambda_param=0.01, n_iters=1000):
+    def __init__(
+        self,
+        learning_rate=0.001,
+        lambda_param=0.01,
+        n_iters=1000,
+        kernel="linear",
+        gamma=0.5,
+    ):
         self.lr = learning_rate
         self.lambda_param = lambda_param
         self.n_iters = n_iters
+        self.kernel = kernel
+        self.gamma = gamma
         self.classifiers = {}
 
     def fit(self, X, y):
@@ -57,14 +113,14 @@ class SVMClassifier:
                 X_pair = X[mask]
                 y_pair = y[mask]
                 y_pair = np.where(y_pair == self.unique[i], 1, -1)
-
                 clf = BinarySVMClassifier(
                     learning_rate=self.lr,
                     n_iters=self.n_iters,
                     lambda_param=self.lambda_param,
+                    kernel=self.kernel,
+                    gamma=self.gamma,
                 )
                 clf.fit(X_pair, y_pair)
-
                 self.classifiers[(self.unique[i], self.unique[j])] = clf
 
     def predict(self, X):
@@ -100,14 +156,25 @@ if __name__ == "__main__":
     pt = PowerTransformer(standardize=True)  # Standard Scaling already included
     X_train_transformed = pt.fit_transform(X_train)
     X_test_transformed = pt.transform(X_test)
-    svm = SVMClassifier()
-    svm.fit(X_train_transformed, y_train)
-    preds = svm.predict(X_test_transformed)
+    print("Linear SVM:")
+    svm_linear = SVMClassifier(kernel="linear")
+    svm_linear.fit(X_train_transformed, y_train)
+    preds_linear = svm_linear.predict(X_test_transformed)
     print("actual:")
     print(y_test)
     print("preds:")
-    print(preds)
-    print(accuracy_score(y_test, preds))
+    print(preds_linear)
+    print(accuracy_score(y_test, preds_linear))
+
+    print("RBF SVM:")
+    svm_rbf = SVMClassifier(kernel="rbf", gamma=0.5)
+    svm_rbf.fit(X_train_transformed, y_train)
+    preds_rbf = svm_rbf.predict(X_test_transformed)
+    print("actual:")
+    print(y_test)
+    print("preds:")
+    print(preds_rbf)
+    print(accuracy_score(y_test, preds_rbf))
 
     sksvm = SGDClassifier(loss="hinge")
     sksvm.fit(X_train_transformed, y_train)
